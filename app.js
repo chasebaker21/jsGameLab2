c8c9318ca6984c8e9c655cb935fb9cc561df2c61
chase <chasebaker21@gmail.com>
2019-11-18 17:26:25 -0500

    removed async defer from both js links in html

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 "use strict";
 
+// create the random number between 1 and 10 / used for damage
 const randomDamage = () => Math.floor(Math.random() * 10) + 1;
 
 // arrow function that creates random number 1 or 0, than returns opt1 or opt2
 const chooseOption = (opt1, opt2) => {
   let randNum = Math.round(Math.random());
-     opt1 = 0;
-     opt2 = 1;
   return randNum == 0 ? opt1 : opt2;
 };
 
@@ -16,13 +15,10 @@ const attackPlayer = function(health) {
 };
 
 // will show the health of the damaged player
-const logHealth = (player, health) => {
+const logHealth = (player, health) =>
   console.log(`${player} health: ${health}`);
-};
 
-const logDeath = (winner, loser) => {
-  console.log(`${winner} defeated ${loser}`);
-};
+const logDeath = (winner, loser) => console.log(`${winner} defeated ${loser}`);
 
 const isDead = health => {
   if (health <= 0) {
@@ -32,40 +28,27 @@ const isDead = health => {
   }
 };
 
-function fight(player1, player1Health, player2, player2Health) {    
+function fight(player1, player2, player1Health, player2Health) {
   while (true) {
     // player one will attack player twos health
     let attacker = chooseOption(player1, player2);
-
-    if (attacker == player1) {
-      player2Health == attackPlayer(player2Health);
-    } else {
-         logHealth(player2, player2Health);
-      break;
-    }
-
+    if (attacker === player1) {
+      player2Health = attackPlayer(player2Health);
+      logHealth(player2, player2Health);
     // will log the winner and loser otherwise will damage player 2
     if (isDead(player2Health)) {
       logDeath(player1, player2);
       break;
     } else {
-      player1Health == attackPlayer(player1Health);
+      player1Health = attackPlayer(player1Health);
       logHealth(player1, player1Health);
     }
 
     // will declare the winner and loser if player ones health falls below zero
-    if (isDead === player1Health) {
+    if (isDead(player1Health)) {
       logDeath(player2, player1);
       break;
     }
   }
 }
-
- // fight();
-console.log(randomDamage());
-
-console.log(attackPlayer());
-
-console.log(logHealth());
-
-// window.fight=fight;
+}
